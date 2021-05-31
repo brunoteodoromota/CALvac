6142e6f34a517b022901b0fe518a145d4a3c0a4a
brunoteodoromota <bruno.teomota@gmail.com>
2021-05-31 18:07:18 -0300

    Finalização do Calendário Adolescente
    Finaliza a criação do Calendário Adolescente
    
    Co-Authored-By: lucas0806 <50914573+lucas0806@users.noreply.github.com>
    Co-Authored-By: Alessandro601 <34521064+Alessandro601@users.noreply.github.com>

--- app/src/main/java/com/example/calvac/Cal2AdolescenteActivity.kt
+++ app/src/main/java/com/example/calvac/Cal2AdolescenteActivity.kt
@@ -1,11 +1,22 @@
 package com.example.calvac
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Cal2AdolescenteActivity : AppCompatActivity() {
+    private lateinit var buttonVoltar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cal2_adolescente)
+
+        buttonVoltar = findViewById(R.id.buttonVoltarAdolescente)
+
+        buttonVoltar.setOnClickListener{
+            val intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/calvac/MenuPrincipalActivity.kt
+++ app/src/main/java/com/example/calvac/MenuPrincipalActivity.kt
@@ -8,12 +8,15 @@ import android.widget.Button
 class MenuPrincipalActivity : AppCompatActivity() {
     private lateinit var buttonSair: Button
     private lateinit var buttonCrianca: Button
+    private lateinit var buttonAdolescente: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
 
         buttonSair = findViewById(R.id.button_sair) as Button
         buttonCrianca = findViewById(R.id.button_crianca) as Button
+        buttonAdolescente = findViewById(R.id.button_adolescente) as Button
 
         buttonSair.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -22,6 +25,11 @@ class MenuPrincipalActivity : AppCompatActivity() {
 
         buttonCrianca.setOnClickListener {
             val intent = Intent(this, Cal1CriancaActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonAdolescente.setOnClickListener {
+            val intent = Intent(this, Cal2AdolescenteActivity::class.java)
             startActivity(intent)
         }
 
