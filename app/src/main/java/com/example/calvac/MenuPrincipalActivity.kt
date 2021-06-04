e7c8e53e2bcd62c852512729e345478fc96af454
lucas0806 <50914573+lucas0806@users.noreply.github.com>
2021-06-03 21:36:30 -0300

    Criação de Calendário Gestante e Correção em Tela Recuperar Senha
    Inicia e finaliza a criação do Calendário Gestante e corrige um textview na tela Recuperar Senha
    
    Co-Authored-By: Alessandro601 <34521064+Alessandro601@users.noreply.github.com>
    Co-Authored-By: brunoteodoromota <45887875+brunoteodoromota@users.noreply.github.com>

--- app/src/main/java/com/example/calvac/MenuPrincipalActivity.kt
+++ app/src/main/java/com/example/calvac/MenuPrincipalActivity.kt
@@ -11,6 +11,7 @@ class MenuPrincipalActivity : AppCompatActivity() {
     private lateinit var buttonAdolescente: Button
     private lateinit var buttonAdulto: Button
     private lateinit var buttonIdoso: Button
+    private lateinit var buttonGestante: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,7 @@ class MenuPrincipalActivity : AppCompatActivity() {
         buttonAdolescente = findViewById(R.id.button_adolescente)
         buttonAdulto = findViewById(R.id.button_adulto)
         buttonIdoso = findViewById(R.id.button_idoso)
+        buttonGestante = findViewById(R.id.button_gestante)
 
         buttonSair.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -47,6 +49,10 @@ class MenuPrincipalActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        buttonGestante.setOnClickListener{
+            val intent = Intent (this, Cal5GestanteActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
